c1fd38a2a58fc9518f7515116c5bfcfd8b745402
Rasmus <rasmus.raasuke@gmail.com>
2026-08-05 16:14:21 +0300

    introduce FollowupRoutes

--- code/xml-mapper/src/Launcher.kt
+++ code/xml-mapper/src/Launcher.kt
@@ -1,4 +1,5 @@
 import dataset.DatasetRoutes
+import followup.FollowupRoutes
 import klite.Config
 import klite.Server
 import klite.annotations.annotated
@@ -19,26 +20,7 @@ fun main() {
       annotated<UploadRoutes>("/upload")
       annotated<SearchRoutes>("/search")
       annotated<DatasetRoutes>("/dataset")
-
-      post("/followup/request-to-xml") {
-        // body: UIL+Message as json
-        // return: FTI025LodgeFollowUpCommRequest as xml
-      }
-
-      post("/followup/request-to-json") {
-        // body: FTI025LodgeFollowUpCommRequest as xml
-        // return: UIL+Message as json
-      }
-
-      post("/followup/response-to-json") { // do we need this at all?
-        // body: FTI030LodgeFollowUpCommResponse as xml
-        // return: UIL as json
-      }
-
-      post("/followup/response-to-xml") { // do we need this at all?
-        // body: UIL as json
-        // return: FTI030LodgeFollowUpCommResponse as xml
-      }
+      annotated<FollowupRoutes>("/followup")
     }
 
     start()
