a522055f5ad17c2b4ba92e481a132129d88cec15
Rihard Rivis <rihard.rivis@proton.me>
2026-08-13 13:02:41 +0300

    use TEDI for the toasts
    toast should be fixed w. and have the necessary stiles

--- code/ui/src/components/Toasts.test.ts
+++ code/ui/src/components/Toasts.test.ts
@@ -5,17 +5,18 @@ import {showToast, ToastType} from 'src/stores/toasts'
 describe('Toasts', () => {
   it('renders success toast', async () => {
     const {container} = render(Toasts)
-    await act(() => showToast('Great success', {type: ToastType.SUCCESS}))
+    await act(() => showToast('Great success', {type: ToastType.SUCCESS, title: 'Success'}))
     const toast = container.querySelector('.toast')!
     expect(toast.textContent).to.contain('Great success')
-    expect(toast.querySelector('.text-success-600')).to.exist
+    expect(toast.textContent).to.contain('Success')
   })
 
-  it('renders warning toast', async () => {
+  it('renders warning toast with icon', async () => {
     const {container} = render(Toasts)
-    await act(() => showToast('Great warning', {type: ToastType.WARNING}))
+    await act(() => showToast('Great warning', {type: ToastType.WARNING, title: 'Warning'}))
     const toast = [...container.querySelectorAll('.toast')].last()
     expect(toast.textContent).to.contain('Great warning')
-    expect(toast.querySelector('.text-warning-600')).to.exist
+    expect(toast.textContent).to.contain('Warning')
+    expect(toast.querySelector('svg')).to.exist
   })
 })
