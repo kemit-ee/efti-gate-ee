bd53c57384defa75d2bf4a60af51aaa683f8ed6d
Anton Keks <anton@azib.net>
2026-08-17 14:21:27 +0300

    do not return deleted gates

--- DSL/Resql/efti/POST/get_gates.sql
+++ DSL/Resql/efti/POST/get_gates.sql
@@ -1,14 +1,17 @@
-SELECT DISTINCT ON (id)
-  row_id,
-  id,
-  country_code,
-  e_delivery_url,
-  e_delivery_cert,
-  tls_cert,
-  status::text,
-  last_ping_at,
-  is_active AS is_gate_active,
-  created_at
-FROM gates
-ORDER BY id, created_at DESC
+SELECT * FROM (
+  SELECT DISTINCT ON (id)
+    row_id,
+    id,
+    country_code,
+    e_delivery_url,
+    e_delivery_cert,
+    tls_cert,
+    status::text,
+    last_ping_at,
+    is_active AS is_gate_active,
+    created_at
+  FROM gates
+  ORDER BY id, created_at DESC
+) latest
+WHERE status != 'DELETED'
 LIMIT COALESCE(:limit::int, 20) OFFSET COALESCE(:offset::int, 0);
