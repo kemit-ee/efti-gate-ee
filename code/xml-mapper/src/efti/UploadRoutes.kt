5ca6180595c18cf82df68131b0cb2adfd689f43f
Anton Keks <anton@azib.net>
2026-08-18 11:22:12 +0300

    let UploadRoutes return a Consignment as json

--- code/xml-mapper/src/efti/UploadRoutes.kt
+++ code/xml-mapper/src/efti/UploadRoutes.kt
@@ -6,6 +6,8 @@ import io.swagger.v3.oas.annotations.Operation
 import io.swagger.v3.oas.annotations.tags.Tag
 import klite.HttpExchange
 import klite.annotations.POST
+import klite.json.toJsonValues
+import klite.nodes.Node
 import klite.uuid
 
 @Tag(
@@ -13,13 +15,15 @@ import klite.uuid
   description = "These routes are for mapping requests and responses for consignment uploads."
 )
 class UploadRoutes(val requestIdHandler: RequestIdHandler) {
-  @Operation(description = "Map FTI004UploadIdentifierRequest or UniqueIDSetUIL as XML to UniqueIDSetUIL as JSON.")
-  @POST("/request-to-json") fun requestToJson(xml: String, e: HttpExchange): UniqueIDSetUIL =
-    if (xml.contains("FTI004UploadIdentifierRequest")) {
+  @Operation(description = "Map FTI004UploadIdentifierRequest or UniqueIDSetUIL as XML to a flat consignment json suitable for DB insertion.")
+  @POST("/request-to-json") fun requestToJson(xml: String, e: HttpExchange): Node {
+    val content = if (xml.contains("FTI004UploadIdentifierRequest")) {
       val req = xmlParser.parse<FTI004UploadIdentifierRequest>(xml)
       requestIdHandler.send(e, req.document.queryId)
       req.content
     } else xmlParser.parse<UniqueIDSetUIL>(xml)
+    return (content.uil.toJsonValues() + content.criteria.toJsonValues()).mapKeys { it.key.name } + ("xml" to xml)
+  }
 
   @Operation(description = "Map UIL as JSON to FTI029UploadIdentifierResponse as XML.")
   @POST("/response-to-xml") fun responseToXml(uil: UIL, e: HttpExchange): String =
--- code/xml-mapper/test/efti/UploadRoutesTest.kt
+++ code/xml-mapper/test/efti/UploadRoutesTest.kt
@@ -4,7 +4,7 @@ import ch.tutteli.atrium.api.fluent.en_GB.toContain
 import ch.tutteli.atrium.api.fluent.en_GB.toEqual
 import ch.tutteli.atrium.api.verbs.expect
 import efti.domain.GateId
-import efti.domain.Mode
+import efti.domain.Mode.Companion.MARINE
 import efti.domain.PlatformId
 import efti.domain.UIL
 import efti.subsets.CountryCode.DE
@@ -15,7 +15,7 @@ import klite.uuid
 import org.junit.jupiter.api.Test
 import java.io.File
 
-class UploadRoutesTest : BaseMocks() {
+class UploadRoutesTest: BaseMocks() {
   val routes = UploadRoutes(requestIdHandler)
   val uil = UIL(PlatformId("demo"), "550e8400-e29b-41d4-a716-446655440000".uuid, GateId("POC"))
 
@@ -25,9 +25,11 @@ class UploadRoutesTest : BaseMocks() {
 
     verify { requestIdHandler.send(exchange, "17022113-89b5-11f1-bec0-3c9c0f2eb459".uuid) }
 
-    expect(result.uil).toEqual(uil)
-    expect(result.criteria.acceptanceCountry).toEqual(DE)
-    expect(result.criteria.transportMode).toEqual(Mode("1"))
+    expect(result["datasetId"]).toEqual(uil.datasetId)
+    expect(result["gateId"]).toEqual(uil.gateId)
+    expect(result["platformId"]).toEqual(uil.platformId)
+    expect(result["acceptanceCountry"]).toEqual(DE)
+    expect(result["transportMode"]).toEqual(MARINE)
   }
 
   @Test fun responseToXml() {
