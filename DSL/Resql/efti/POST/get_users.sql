9fd31adf0e9cb9b04ea571fa19953e49420f4e2d
Rihard Rivis <rihard.rivis@proton.me>
2026-08-24 14:18:05 +0300

    get_users return only users in active state

--- DSL/Resql/efti/POST/get_users.sql
+++ DSL/Resql/efti/POST/get_users.sql
@@ -1,11 +1,14 @@
-SELECT DISTINCT ON (id)
-  row_id,
-  id,
-  tara_sub,
-  name,
-  token_revoked_at,
-  is_active AS is_user_active,
-  created_at
-FROM users
-ORDER BY id, created_at DESC
+SELECT * FROM (
+  SELECT DISTINCT ON (id)
+    row_id,
+    id,
+    tara_sub,
+    name,
+    token_revoked_at,
+    is_active AS is_user_active,
+    created_at
+  FROM users
+  ORDER BY id, created_at DESC
+) latest
+WHERE is_user_active = true
 LIMIT COALESCE(:limit::int, 20) OFFSET COALESCE(:offset::int, 0);
