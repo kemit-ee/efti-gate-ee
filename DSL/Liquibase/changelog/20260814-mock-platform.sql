cfb88858ea0756ccb839703535c2bd3cbd0e6acd
Anton Keks <anton@azib.net>
2026-08-25 13:26:43 +0300

    fix mock platform

--- DSL/Liquibase/changelog/20260814-mock-platform.sql
+++ DSL/Liquibase/changelog/20260814-mock-platform.sql
@@ -2,6 +2,6 @@
 
 --changeset efti:013-seed-mock-platform context:dev
 INSERT INTO platforms (id, base_url, headers) VALUES
-  ('mock', 'http://ruuter:8086/efti/mock-platform', '{"X-Api-Key": "mock-secret-key"}'::jsonb);
+  ('mock', 'http://ruuter:8086/mock-platform', '{"X-Api-Key": "mock-secret-key"}'::jsonb);
 
 --rollback DELETE FROM platforms WHERE id = 'mock';
