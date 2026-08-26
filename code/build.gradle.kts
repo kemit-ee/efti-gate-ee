6077a8d33108600d3a52a19ca18ce8f6942bc8b6
Anton Keks <anton@azib.net>
2026-08-26 17:36:52 +0300

    update klite

--- code/build.gradle.kts
+++ code/build.gradle.kts
@@ -22,7 +22,7 @@ subprojects {
   }
 
   dependencies {
-    fun klite(module: String) = "com.github.keksworks.klite:klite-$module:2.0.1"
+    fun klite(module: String) = "com.github.keksworks.klite:klite-$module:2.0.2"
     implementation(klite("server"))
     implementation(klite("json"))
     implementation(klite("xml"))
