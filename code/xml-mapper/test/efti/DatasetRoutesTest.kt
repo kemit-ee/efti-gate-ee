8250bff3ea53074b1e15516d5f4ae1d11bb6d18e
Rasmus <rasmus.raasuke@gmail.com>
2026-08-27 17:51:42 +0300

    update expected value for platformId

--- code/xml-mapper/test/efti/DatasetRoutesTest.kt
+++ code/xml-mapper/test/efti/DatasetRoutesTest.kt
@@ -22,8 +22,8 @@ class DatasetRoutesTest : BaseMocks() {
 
     verify { requestIdHandler.send(exchange, "17022113-89b5-11f1-bec0-3c9c0f2eb459".uuid) }
 
-    expect(result.uil.gateId).toEqual(GateId("Gate-001"))
-    expect(result.uil.platformId).toEqual(PlatformId("Platform-001"))
+    expect(result.uil.gateId).toEqual(GateId("EU-EE"))
+    expect(result.uil.platformId).toEqual(PlatformId("mock"))
     expect(result.uil.datasetId).toEqual("550e8400-e29b-41d4-a716-446655440000".uuid)
     expect(result.subsets).toEqual(listOf(Subset("EE05b")))
   }
--- code/xml-mapper/test/efti/xml/fti/FTIMessagesTest.kt
+++ code/xml-mapper/test/efti/xml/fti/FTIMessagesTest.kt
@@ -65,8 +65,8 @@ class FTIMessagesTest {
     expect(req.context).toEqual(context)
     expect(req.document).toEqual(document.copy(typeCode = "009", requesterCountry = DE))
     expect(req.subsets).toEqual(listOf(Subset("EE05b")))
-    expect(req.uil.gateId).toEqual(GateId("Gate-001"))
-    expect(req.uil.platformId).toEqual(PlatformId("Platform-001"))
+    expect(req.uil.gateId).toEqual(GateId("EU-EE"))
+    expect(req.uil.platformId).toEqual(PlatformId("mock"))
     expect(req.uil.datasetId).toEqual("550e8400-e29b-41d4-a716-446655440000".uuid)
 
     val rendered = req.render()
