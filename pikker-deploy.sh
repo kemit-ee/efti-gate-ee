6ab46e307c77edab133807aa4b346b0fbd0d6361
Rihard Rivis <rihard.rivis@proton.me>
2026-09-03 10:42:48 +0300

    better deployment script - build images in local pc
    Building inside Hetzner server takes a long time. in local PC it is faster to deploy

--- pikker-deploy.sh
+++ pikker-deploy.sh
@@ -3,10 +3,20 @@ set -e
 
 SERVER=root@pikker.dev
 
+DOCKER_DEFAULT_PLATFORM=linux/amd64 docker compose -f compose.yml -f compose.pikker.yml build
+
+IMAGES=$(docker compose images --format json | jq -r '.[] | select(.Repository != "sha256") | "\(.Repository):\(.Tag)"' | sort -u)
+
+echo "Saving images: $IMAGES"
+docker save $IMAGES | gzip | ssh $SERVER 'gunzip | docker load'
+
+scp compose.yml compose.pikker.yml $SERVER:efti-gate-ee/
+
 ssh -t $SERVER << 'END'
   set -e
   cd efti-gate-ee
-  GIT_TERMINAL_PROMPT=0 git pull
-  docker compose -f compose.yml -f compose.pikker.yml up --build -d --wait
-  docker compose logs --tail=50
+  mkdir -p logs
+  docker compose -f compose.yml -f compose.pikker.yml logs -t > logs/deploy-$(date --iso-8601=minutes).log 2>&1 || true
+  docker compose -f compose.yml -f compose.pikker.yml up -d --wait
+  docker compose -f compose.yml -f compose.pikker.yml logs -f
 END
