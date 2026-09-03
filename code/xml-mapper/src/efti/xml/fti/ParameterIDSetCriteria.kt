744902c61ec7db538019fc81d6812af09eb62672
Rasmus <rasmus.raasuke@gmail.com>
2026-09-03 16:06:15 +0300

    fix ParameterIDSetCriteria rendering and saving to database

--- code/xml-mapper/src/efti/xml/fti/ParameterIDSetCriteria.kt
+++ code/xml-mapper/src/efti/xml/fti/ParameterIDSetCriteria.kt
@@ -11,10 +11,8 @@ data class UniqueIDSetUniqueIDSet(
   @XmlPath("") val uil: UIL,
   @XmlPath("ParameterIDSetCriteria") val criteria: ParameterIDSetCriteria? = null
 ) {
-  @Language("xml") fun render(): String {
-    val criteriaXml = ParameterIDSetCriteria.render(criteria)
-    return """<UniqueIDSetUniqueIDSet><GateID>${uil.gateId}</GateID><PlatformID>${uil.platformId}</PlatformID><DatasetID schemeID="RFC 9562-4">${uil.datasetId}</DatasetID>${if (criteriaXml.isNotEmpty()) "<ParameterIDSetCriteria>$criteriaXml</ParameterIDSetCriteria>" else ""}</UniqueIDSetUniqueIDSet>"""
-  }
+  @Language("xml") fun render(criteriaXml: String = criteria?.render() ?: ""): String =
+    """<UniqueIDSetUniqueIDSet><GateID>${uil.gateId}</GateID><PlatformID>${uil.platformId}</PlatformID><DatasetID schemeID="RFC 9562-4">${uil.datasetId}</DatasetID>$criteriaXml</UniqueIDSetUniqueIDSet>"""
 }
 
 /** Upload data */
@@ -48,85 +46,84 @@ data class ParameterIDSetCriteria(
   @XmlPath("CarriedTransportEquipmentCategoryCodeParameterScope/TransportEquipmentCategoryParameterCode") val carriedEquipmentCategories: List<String> = emptyList(),
   @XmlPath("CarriedTransportEquipmentSequenceNumberParameterScope/SequenceNumeric") val carriedEquipmentSeq: List<Int> = emptyList(),
 ) {
-  companion object {
-    fun render(criteria: ParameterIDSetCriteria?): String {
-      if (criteria == null) return ""
-      return buildString {
-        criteria.acceptanceDate?.let {
-          append("<CarrierAcceptanceDateParameterScope><SpecifiedDateTime>${it.render()}</SpecifiedDateTime></CarrierAcceptanceDateParameterScope>")
-        }
-        criteria.acceptanceCountry?.let {
-          append("<CarrierAcceptanceCountryParameterScope><CountryID>$it</CountryID></CarrierAcceptanceCountryParameterScope>")
-        }
-        criteria.deliveryDate?.let {
-          append("<DeliveryDateParameterScope><SpecifiedDateTime>${it.render()}</SpecifiedDateTime></DeliveryDateParameterScope>")
-        }
-        criteria.deliveryCountry?.let {
-          append("<DeliveryCountryParameterScope><CountryID>$it</CountryID></DeliveryCountryParameterScope>")
-        }
-        criteria.dangerousGoods?.let {
-          append("<DangerousGoodsIndicationCodeParameterScope><DangerousGoodsIndicationParameterCode>$it</DangerousGoodsIndicationParameterCode></DangerousGoodsIndicationCodeParameterScope>")
-        }
-        criteria.mainTransportId?.let {
-          append("<MainCarriageTransportMeansIDParameterScope><ID>$it</ID></MainCarriageTransportMeansIDParameterScope>")
-        }
-        criteria.transportMode?.let {
-          append("<MainCarriageModeCodeParameterScope><TransportModeParameterCode>$it</TransportModeParameterCode></MainCarriageModeCodeParameterScope>")
-        }
-        criteria.mainTransportType?.let {
-          append("<MainCarriageTransportMeansTypeCodeParameterScope><TransportMeansParameterCode>$it</TransportMeansParameterCode></MainCarriageTransportMeansTypeCodeParameterScope>")
-        }
-        criteria.transportRegCountry?.let {
-          append("<TransportMeansRegistrationCountryParameterScope><CountryID>$it</CountryID></TransportMeansRegistrationCountryParameterScope>")
-        }
-        criteria.loadingDate?.let {
-          append("<MainCarriageLoadingDateParameterScope><SpecifiedDateTime>${it.render()}</SpecifiedDateTime></MainCarriageLoadingDateParameterScope>")
-        }
-        criteria.loadingCountry?.let {
-          append("<MainCarriageLoadingCountryParameterScope><CountryID>$it</CountryID></MainCarriageLoadingCountryParameterScope>")
-        }
-        criteria.unloadingDate?.let {
-          append("<MainCarriageUnloadingDateParameterScope><SpecifiedDateTime>${it.render()}</SpecifiedDateTime></MainCarriageUnloadingDateParameterScope>")
-        }
-        criteria.unloadingCountry?.let {
-          append("<MainCarriageUnloadingCountryParameterScope><CountryID>$it</CountryID></MainCarriageUnloadingCountryParameterScope>")
-        }
-        if (criteria.usedEquipmentIds.isNotEmpty()) {
-          append("<UsedTransportEquipmentIDParameterScope>")
-          criteria.usedEquipmentIds.forEach { id -> append("<ID>$id</ID>") }
-          append("</UsedTransportEquipmentIDParameterScope>")
-        }
-        if (criteria.usedEquipmentCategories.isNotEmpty()) {
-          append("<UsedTransportEquipmentCategoryCodeParameterScope>")
-          criteria.usedEquipmentCategories.forEach { code -> append("<TransportEquipmentCategoryParameterCode>$code</TransportEquipmentCategoryParameterCode>") }
-          append("</UsedTransportEquipmentCategoryCodeParameterScope>")
-        }
-        if (criteria.usedEquipmentCountries.isNotEmpty()) {
-          append("<UsedTransportEquipmentRegistrationCountryParameterScope>")
-          criteria.usedEquipmentCountries.forEach { cc -> append("<CountryID>$cc</CountryID>") }
-          append("</UsedTransportEquipmentRegistrationCountryParameterScope>")
-        }
-        if (criteria.usedEquipmentSeq.isNotEmpty()) {
-          append("<UsedTransportEquipmentSequenceNumberParameterScope>")
-          criteria.usedEquipmentSeq.forEach { seq -> append("<SequenceNumeric>$seq</SequenceNumeric>") }
-          append("</UsedTransportEquipmentSequenceNumberParameterScope>")
-        }
-        if (criteria.carriedEquipmentIds.isNotEmpty()) {
-          append("<CarriedTransportEquipmentIDParameterScope>")
-          criteria.carriedEquipmentIds.forEach { id -> append("<ID>$id</ID>") }
-          append("</CarriedTransportEquipmentIDParameterScope>")
-        }
-        if (criteria.carriedEquipmentCategories.isNotEmpty()) {
-          append("<CarriedTransportEquipmentCategoryCodeParameterScope>")
-          criteria.carriedEquipmentCategories.forEach { code -> append("<TransportEquipmentCategoryParameterCode>$code</TransportEquipmentCategoryParameterCode>") }
-          append("</CarriedTransportEquipmentCategoryCodeParameterScope>")
-        }
-        if (criteria.carriedEquipmentSeq.isNotEmpty()) {
-          append("<CarriedTransportEquipmentSequenceNumberParameterScope>")
-          criteria.carriedEquipmentSeq.forEach { seq -> append("<SequenceNumeric>$seq</SequenceNumeric>") }
-          append("</CarriedTransportEquipmentSequenceNumberParameterScope>")
-        }
+  fun render(): String {
+    return buildString {
+      append("<ParameterIDSetCriteria>")
+      acceptanceDate?.let {
+        append("<CarrierAcceptanceDateParameterScope><SpecifiedDateTime>${it.render()}</SpecifiedDateTime></CarrierAcceptanceDateParameterScope>")
       }
+      acceptanceCountry?.let {
+        append("<CarrierAcceptanceCountryParameterScope><CountryID>$it</CountryID></CarrierAcceptanceCountryParameterScope>")
+      }
+      deliveryDate?.let {
+        append("<DeliveryDateParameterScope><SpecifiedDateTime>${it.render()}</SpecifiedDateTime></DeliveryDateParameterScope>")
+      }
+      deliveryCountry?.let {
+        append("<DeliveryCountryParameterScope><CountryID>$it</CountryID></DeliveryCountryParameterScope>")
+      }
+      dangerousGoods?.let {
+        append("<DangerousGoodsIndicationCodeParameterScope><DangerousGoodsIndicationParameterCode>$it</DangerousGoodsIndicationParameterCode></DangerousGoodsIndicationCodeParameterScope>")
+      }
+      mainTransportId?.let {
+        append("<MainCarriageTransportMeansIDParameterScope><ID>$it</ID></MainCarriageTransportMeansIDParameterScope>")
+      }
+      transportMode?.let {
+        append("<MainCarriageModeCodeParameterScope><TransportModeParameterCode>$it</TransportModeParameterCode></MainCarriageModeCodeParameterScope>")
+      }
+      mainTransportType?.let {
+        append("<MainCarriageTransportMeansTypeCodeParameterScope><TransportMeansParameterCode>$it</TransportMeansParameterCode></MainCarriageTransportMeansTypeCodeParameterScope>")
+      }
+      transportRegCountry?.let {
+        append("<TransportMeansRegistrationCountryParameterScope><CountryID>$it</CountryID></TransportMeansRegistrationCountryParameterScope>")
+      }
+      loadingDate?.let {
+        append("<MainCarriageLoadingDateParameterScope><SpecifiedDateTime>${it.render()}</SpecifiedDateTime></MainCarriageLoadingDateParameterScope>")
+      }
+      loadingCountry?.let {
+        append("<MainCarriageLoadingCountryParameterScope><CountryID>$it</CountryID></MainCarriageLoadingCountryParameterScope>")
+      }
+      unloadingDate?.let {
+        append("<MainCarriageUnloadingDateParameterScope><SpecifiedDateTime>${it.render()}</SpecifiedDateTime></MainCarriageUnloadingDateParameterScope>")
+      }
+      unloadingCountry?.let {
+        append("<MainCarriageUnloadingCountryParameterScope><CountryID>$it</CountryID></MainCarriageUnloadingCountryParameterScope>")
+      }
+      if (usedEquipmentIds.isNotEmpty()) {
+        append("<UsedTransportEquipmentIDParameterScope>")
+        usedEquipmentIds.forEach { id -> append("<ID>$id</ID>") }
+        append("</UsedTransportEquipmentIDParameterScope>")
+      }
+      if (usedEquipmentCategories.isNotEmpty()) {
+        append("<UsedTransportEquipmentCategoryCodeParameterScope>")
+        usedEquipmentCategories.forEach { code -> append("<TransportEquipmentCategoryParameterCode>$code</TransportEquipmentCategoryParameterCode>") }
+        append("</UsedTransportEquipmentCategoryCodeParameterScope>")
+      }
+      if (usedEquipmentCountries.isNotEmpty()) {
+        append("<UsedTransportEquipmentRegistrationCountryParameterScope>")
+        usedEquipmentCountries.forEach { cc -> append("<CountryID>$cc</CountryID>") }
+        append("</UsedTransportEquipmentRegistrationCountryParameterScope>")
+      }
+      if (usedEquipmentSeq.isNotEmpty()) {
+        append("<UsedTransportEquipmentSequenceNumberParameterScope>")
+        usedEquipmentSeq.forEach { seq -> append("<SequenceNumeric>$seq</SequenceNumeric>") }
+        append("</UsedTransportEquipmentSequenceNumberParameterScope>")
+      }
+      if (carriedEquipmentIds.isNotEmpty()) {
+        append("<CarriedTransportEquipmentIDParameterScope>")
+        carriedEquipmentIds.forEach { id -> append("<ID>$id</ID>") }
+        append("</CarriedTransportEquipmentIDParameterScope>")
+      }
+      if (carriedEquipmentCategories.isNotEmpty()) {
+        append("<CarriedTransportEquipmentCategoryCodeParameterScope>")
+        carriedEquipmentCategories.forEach { code -> append("<TransportEquipmentCategoryParameterCode>$code</TransportEquipmentCategoryParameterCode>") }
+        append("</CarriedTransportEquipmentCategoryCodeParameterScope>")
+      }
+      if (carriedEquipmentSeq.isNotEmpty()) {
+        append("<CarriedTransportEquipmentSequenceNumberParameterScope>")
+        carriedEquipmentSeq.forEach { seq -> append("<SequenceNumeric>$seq</SequenceNumeric>") }
+        append("</CarriedTransportEquipmentSequenceNumberParameterScope>")
+      }
+      append("</ParameterIDSetCriteria>")
     }
   }
 }
