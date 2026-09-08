cee12d3952e9e3eb28b7987cf69752301c6e363f
Rihard Rivis <rihard.rivis@proton.me>
2026-09-08 12:39:53 +0300

    fix request id missing issue. use random uuid if missing

--- code/core/src/RequestIdHandler.kt
+++ code/core/src/RequestIdHandler.kt
@@ -7,7 +7,7 @@ private const val requestIdHeader = "x-request-id"
 
 class RequestIdHandler: RequestIdGenerator() {
   override fun invoke(headers: Headers) =
-    headers.getFirst(requestIdHeader) ?: UUID.randomUUID().toString()
+    headers.getFirst(requestIdHeader)?.takeIf { it.isNotEmpty() } ?: UUID.randomUUID().toString()
 
   fun send(e: HttpExchange, requestId: UUID) =
     e.header(requestIdHeader, requestId.toString())
